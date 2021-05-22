f77557fff7c27129d8d8761127d045548cce1277
arafat1419 <arafat1419@gmail.com>
2021-05-22 18:08:18 +0800

    small changes at libraries

--- mobile/app/src/main/java/com/bangkitcapstone/coral_id/ui/home/HomeActivity.kt
+++ mobile/app/src/main/java/com/bangkitcapstone/coral_id/ui/home/HomeActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import com.bangkitcapstone.coral_id.R
 
 class HomeActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
