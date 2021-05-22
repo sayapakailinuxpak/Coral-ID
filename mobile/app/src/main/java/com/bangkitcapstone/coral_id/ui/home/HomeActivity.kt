115ca15106313f8a8aec707172ecfb46ae27012e
sayapakailinuxpak <sayapakailinuxpak@gmail.com>
2021-05-22 17:00:36 +0700

    create packages

--- mobile/app/src/main/java/com/bangkitcapstone/coral_id/ui/home/HomeActivity.kt
+++ mobile/app/src/main/java/com/bangkitcapstone/coral_id/ui/home/HomeActivity.kt
@@ -1,7 +1,8 @@
-package com.bangkitcapstone.coral_id
+package com.bangkitcapstone.coral_id.ui.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bangkitcapstone.coral_id.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
